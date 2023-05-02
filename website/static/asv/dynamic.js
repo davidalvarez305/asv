1e869145ab0b734603e6a7557c78c3ac12073f38
unknown <david@southfloridaathleticclub.com>
2023-05-02 12:22:55 -0400

    dynamic filters functionality

--- website/static/asv/dynamic.js
+++ website/static/asv/dynamic.js
@@ -5,13 +5,28 @@ class DynamicFilter {
   };
   constructor(inputs) {
     this.formInputs.originalInputs = inputs;
+  }
 
+  parseForm() {
+    const form = document.getElementById("filter-trucks-form");
+
+    const values = Object.fromEntries(new FormData(form));
+
+    for (let i = 0; i < Object.keys(values).length; i++) {
+        const select = document.getElementById(`${values[i]}`);
+        const options = select.options.map((option) => option.innerHTML);
+        this.formInputs.originalInputs[values[i]] = options;
+    };
+  }
+
+  changeFilters() {
     for (const [key, value] of Object.entries(this.formInputs.originalInputs)) {
-      const options = this.createOptionsFactory(value);
-      const el = document.getElementById(`${key}`);
-
-      if (el) el.replaceChildren(...options);
-    }
+        this.formInputs.currentInputs[key] = value;
+        const options = this.createOptionsFactory(value);
+        const el = document.getElementById(`${key}`);
+  
+        if (el) el.replaceChildren(...options);
+      }
   }
 
   static createOptionsFactory(inputs) {
