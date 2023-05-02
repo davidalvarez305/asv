c113db1ab1f047aa94a5aed8fc155600e18e95e4
david alvarez <david@southfloridaathleticclub.com>
2023-05-02 01:24:12 -0400

    download script

---
+++ download.py
@@ -0,0 +1,22 @@
+from base64 import decodebytes
+from posixpath import abspath
+import datetime as dt
+import paramiko
+import os
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv()
+    FILE_NAME = format(dt.date.today().replace(day=1) - dt.timedelta(days=1), '%B_%Y.csv')
+    LOCAL_PATH = abspath('./website/uploads/' + FILE_NAME)
+
+    with paramiko.SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(os.environ.get('FTP_HOST'), username=os.environ.get('FTP_USERNAME'), password=os.environ.get('FTP_PASSWORD'))
+    
+        sftp = ssh.open_sftp()
+
+        sftp.get(remotepath=os.environ.get('FTP_PATH'), localpath=LOCAL_PATH)
+
+if __name__ == "__main__":
+    main()
--- website/asv/utils/upload_file.py
+++ website/asv/utils/upload_file.py
@@ -1,5 +1,4 @@
 from os.path import abspath
-from datetime import date, timedelta
 import datetime as dt
 import csv
 
