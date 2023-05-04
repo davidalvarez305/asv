63a7b53c4b3ae366229b1cb67af08dbcc4db9d45
david alvarez <david@southfloridaathleticclub.com>
2023-05-03 22:51:09 -0400

    this will do for now

---
+++ website/asv/utils/bulk_insert_data.py
@@ -0,0 +1,76 @@
+
+from django.db import transaction
+from asv.models import Truck, VehicleCondition, VehicleDetails, Make, Model, Trim, Branch, Sale
+
+def bulk_insert_data(data):
+    for i in range(0, len(data), 1000):
+        
+        rows = data[i:i+1000]
+        trucks_to_create = []
+        
+        with transaction.atomic():
+            for row in rows:
+                sale_date = ""
+
+                trim = None
+
+                if not '\ufeffSale_Date' in row:
+                    sale_date = row.get('Sale_Date')
+                else:
+                    sale_date = row.get('\ufeffSale_Date')
+
+                make = Make.objects.get_or_create(make=str(row.get('Make')).strip())[0]
+                model = Model.objects.get_or_create(model=str(row.get('Model')).strip())[0]
+                model.make.add(make)
+                model.save()
+            
+                if row.get('Trim') is not None:
+                    trim = Trim.objects.get_or_create(trim=row.get('Trim').strip())[0]
+                    trim.model.add(model)
+                    trim.save()
+                
+                vehicle_details = VehicleDetails.objects.create(
+                    year=row.get('Year'),
+                    make=make,
+                    model = model,
+                    trim = trim,
+                    bodytype=row.get('BodyType'),
+                    cabtype=row.get('CabType'),
+                    fueltype=row.get('FuelType'),
+                    enginesize=row.get('EngineSize'),
+                    odometerreadingtypedescription=row.get('OdometerReadingTypeDescription'),
+                    drivelinetype=row.get('DriveLineType'),
+                    vehicle_condition = VehicleCondition.objects.create(
+                        starts_at_checkin=row.get('Starts_At_CheckIn'),
+                        runs_and_drives=row.get('Runs_And_Drives'),
+                        air_bags_deployed=row.get('Air_Bags_Deployed'),
+                        miles=row.get('Miles'),
+                        loss_type=row.get('Loss_Type'),
+                        damage_description_primary=row.get('Damage_Description_Primary'),
+                    ),
+                )
+
+                branch = Branch.objects.create(
+                    branch=row.get('Branch'),
+                    branch_zip_code=row.get('Branch_Zip_Code'),
+                    stateabbreviation=row.get('StateAbbreviation')
+                )
+
+                sale = Sale.objects.create(
+                    sale_date=sale_date,
+                    saleprice=row.get('SalePrice'),
+                    saledocumenttype=row.get('SaleDocumentType'),
+                    branch = branch
+                )
+
+                truck = Truck(
+                    vin=row.get('VIN'),
+                    data_type=row.get('Data_Type'),
+                    offer=row.get('Offer'),
+                    vehicle_details = vehicle_details,
+                    sale = sale,
+                )
+
+                trucks_to_create.append(truck)
+        
+            Truck.objects.bulk_create(trucks_to_create)
--- website/asv/views.py
+++ website/asv/views.py
@@ -4,10 +4,10 @@ from django.views import View
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db import transaction
 
 from asv.models import Truck, VehicleCondition, VehicleDetails, Make, Model, Trim, Branch, Sale
 from asv.utils.upload_file import handle_uploaded_file
+from asv.utils.bulk_insert_data import bulk_insert_data
 
 class BaseView(View):
     domain = str(os.environ.get('DJANGO_DOMAIN'))
@@ -125,81 +125,11 @@ class Upload(LoginRequiredMixin, BaseView):
 
         data = handle_uploaded_file(file)
 
-        transaction.set_autocommit(False)
-
         try:
-            for row in data:
-                sale_date = ""
-                trim = None
-
-                if not '\ufeffSale_Date' in row:
-                    sale_date = row.get('Sale_Date')
-                else:
-                    sale_date = row.get('\ufeffSale_Date')
-
-                make = Make.objects.get_or_create(make=str(row.get('Make')).strip())[0]
-                model = Model.objects.get_or_create(model=str(row.get('Model')).strip())[0]
-                model.make.add(make)
-                model.save()
-            
-                if row.get('Trim') is not None:
-                    trim = Trim.objects.get_or_create(trim=row.get('Trim').strip())[0]
-                    trim.model.add(model)
-                    trim.save()
-                
-                vehicle_details = VehicleDetails.objects.create(
-                    year=row.get('Year'),
-                    make=make,
-                    model = model,
-                    trim = trim,
-                    bodytype=row.get('BodyType'),
-                    cabtype=row.get('CabType'),
-                    fueltype=row.get('FuelType'),
-                    enginesize=row.get('EngineSize'),
-                    odometerreadingtypedescription=row.get('OdometerReadingTypeDescription'),
-                    drivelinetype=row.get('DriveLineType'),
-                    vehicle_condition = VehicleCondition.objects.create(
-                        starts_at_checkin=row.get('Starts_At_CheckIn'),
-                        runs_and_drives=row.get('Runs_And_Drives'),
-                        air_bags_deployed=row.get('Air_Bags_Deployed'),
-                        miles=row.get('Miles'),
-                        loss_type=row.get('Loss_Type'),
-                        damage_description_primary=row.get('Damage_Description_Primary'),
-                    ),
-                )
-
-                branch = Branch.objects.create(
-                    branch=row.get('Branch'),
-                    branch_zip_code=row.get('Branch_Zip_Code'),
-                    stateabbreviation=row.get('StateAbbreviation')
-                )
-
-                sale = Sale.objects.create(
-                    sale_date=sale_date,
-                    saleprice=row.get('SalePrice'),
-                    saledocumenttype=row.get('SaleDocumentType'),
-                    branch = branch
-                )
-
-                truck = Truck(
-                    vin=row.get('VIN'),
-                    data_type=row.get('Data_Type'),
-                    offer=row.get('Offer'),
-                    vehicle_details = vehicle_details,
-                    sale = sale,
-                )
-                
-                truck.save()
-            transaction.commit()
-
-        except Exception as e:
-            transaction.rollback()
-            raise e
-
-        finally:
-            transaction.set_autocommit(True)
-
-        return render(request, self.template_name)
+            bulk_insert_data(data)
+            return render(request, self.template_name)
+        except BaseException:
+            return HttpResponseBadRequest("Bulk insert failed")
     
 class Login(BaseView):
     template_name = 'asv/login.html'
